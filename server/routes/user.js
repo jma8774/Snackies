22b9ea4bd79a654a86c2f4042ef39bfa0580f665
Jia Ming <jma8774@bths.edu>
2021-11-04 20:38:07 -0400

    googleAuth block in try/catch

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,6 +15,7 @@ function cartCount(cart) {
 // Check if user has token, then we can just give front-end the user infos
 router.get('/', jwtAuth.authenticateToken, async function (req, res) {
   try {
+    // req.email is set by jwtAuth.authenticateToken after it has verified the JWT
     const user = await db.User.findOne({email: req.email}).exec()
     userData = {
       id: user.id,
@@ -26,8 +27,8 @@ router.get('/', jwtAuth.authenticateToken, async function (req, res) {
     }
     res.json(userData)
   } catch (err) {
-    console.log('Get User Info Error:\n', err);
-    // console.log(err)
+    console.log('Get User Info Error:\n', err)
+    res.json({error: "Something went wrong"})
   }
 })
 
@@ -37,7 +38,7 @@ router.post('/login', async function (req, res) {
   // Confirm with database
   const user = await db.User.findOne({email: email}).exec()
   if(user === null || user.password !== password) 
-    return res.json();
+    return res.json({error: "Wrong username/password"});
   // Save JWT token in cookies
   const token = jwtAuth.generateAccessToken(email)
   res.cookie('token', token);
@@ -47,28 +48,31 @@ router.post('/login', async function (req, res) {
 
 // Google oauth login 
 router.post('/googleAuth', async function (req, res) {
-  const {tokenId} = req.body
-  // Get profile info from Google using Token
-  const profile = await googleOAuth.verify(tokenId)
-  const userData = {
-    email: profile.email,
-    password: undefined,
-    first_name: profile.given_name,
-    last_name: profile.family_name
+  try {
+    const {tokenId} = req.body
+    // Get profile info from Google using Token
+    const profile = await googleOAuth.verify(tokenId)
+    const userData = {
+      email: profile.email,
+      password: undefined,
+      first_name: profile.given_name,
+      last_name: profile.family_name
+    }
+    // Check if user email exist, if not we make a new user
+    const user = await db.User.findOne({email: profile.email}).exec()
+    if(user === null) {
+      let newUser = new db.User(userData)
+      newUser = await newUser.save()
+    }
+    // Generate token using email and set it 
+    const token = jwtAuth.generateAccessToken(userData.email)
+    res.cookie('token', token);
+    // console.log("Google Login: Generated Token Cookie")
+    res.json({token});
+  } catch (err) {
+    console.log('Google Login Error:\n', err)
+    res.json({error: "Something went wrong"})
   }
-  // Check if user email exist, if not we make a new user
-  const user = await db.User.findOne({email: profile.email}).exec()
-  if(user === null) {
-    let newUser = new db.User(userData)
-    newUser = await newUser.save()
-  }
-  // Generate token using email and set it 
-  if(!userData.email)
-    return res.json();
-  const token = jwtAuth.generateAccessToken(userData.email)
-  res.cookie('token', token);
-  // console.log("Google Login: Generated Token Cookie")
-  res.json({token});
 })
 
 // User signup
