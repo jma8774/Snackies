af7ce0444d42c757498cd052041646f365d027c2
Jia Ming <jma8774@bths.edu>
2021-11-06 17:44:12 -0400

    missed a line

--- server/middleware/googleOAuth.js
+++ server/middleware/googleOAuth.js
@@ -4,6 +4,10 @@ const client = new OAuth2Client("766162828005-efnc6r8ufsla9t4ter3ep7tt1t0q8ulb.a
 
 async function authenticateGoogleToken(req, res, next) {
   const { tokenId } = req.body
+  
+  if (tokenId === undefined) return res.sendStatus(401)
+  
+  // console.log("Authenticate Token:", token.substring(0,7)+"...")
   const ticket = await client.verifyIdToken({
       idToken: tokenId,
       audience: "766162828005-efnc6r8ufsla9t4ter3ep7tt1t0q8ulb.apps.googleusercontent.com",
